0f076cac92d0997d42d3007f70e486240f4e3cb8
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-02-21 09:58:39 -0600

    Move gems pngs out of landscapes dir

--- gems/config.py
+++ gems/config.py
@@ -4,9 +4,8 @@ pkg_root = path.dirname(path.abspath(__file__))
 EXP_ROOT = path.dirname(pkg_root)
 DATA_DIR = path.join(EXP_ROOT, 'data')
 LANDSCAPE_FILES = path.join(pkg_root, 'landscapes')
+GABORS_DIR = path.join(pkg_root, 'gabors')
 
-if not path.isdir(DATA_DIR):
-    mkdir(DATA_DIR)
-
-if not path.isdir(LANDSCAPE_FILES):
-    mkdir(LANDSCAPE_FILES)
+for expected_dir in [DATA_DIR, LANDSCAPE_FILES, GABORS_DIR]:
+    if not path.isdir(expected_dir):
+        mkdir(expected_dir)
--- tasks/landscape.py
+++ tasks/landscape.py
@@ -10,6 +10,10 @@ landscapes_dir = '../data/data-raw/landscapes'
 if not path.isdir(landscapes_dir):
     mkdir(landscapes_dir)
 
+gabors_dir = '../data/inst/extdata'
+if not path.isdir(gabors_dir):
+    mkdir(gabors_dir)
+
 
 @task
 def data(ctx, name, move_to_r_pkg=False):
@@ -29,6 +33,56 @@ def data(ctx, name, move_to_r_pkg=False):
             output = path.join(gems.config.LANDSCAPE_FILES, '{}.csv'.format(name))
 
         landscape.export(output)
+
+
+@task
+def gabors(ctx, name, output=None, move_to_r_pkg=False, open_after=False):
+    """Draw gabors sampled from the landscape.
+
+    Examples:
+
+        $ inv landscape.draw SimpleHill
+
+    """
+    from psychopy import visual
+    from numpy import linspace
+
+    win = visual.Window(size=(800, 800), units='pix', color=(0.6, 0.6, 0.6))
+
+    grid_size = 10
+    positions = linspace(0, 100, grid_size, endpoint=False, dtype='int')
+    grid_positions = list(product(positions, positions))
+
+    gabor_size = 60
+    positions = linspace(0, 100, grid_size, endpoint=False, dtype='int')
+    grid_positions = list(product(positions, positions))
+    stim_positions = gems.create_grid_positions(n_rows=grid_size, n_cols=grid_size,
+                                           win_size=win.size,
+                                           stim_size=gabor_size)
+
+    output_dir = gabors_dir if move_to_r_pkg else gems.config.GABORS_DIR
+
+    landscapes = get_landscapes_from_name(name)
+    for name, landscape in landscapes.items():
+        landscape.grating_stim_kwargs.update({'win': win, 'size': gabor_size})
+        gabors = landscape.get_grid_of_grating_stims(grid_positions)
+
+        for (grid_pos, stim_pos) in zip(grid_positions, stim_positions):
+            gabor = gabors[grid_pos]
+            gabor.pos = stim_pos
+            gabor.draw()
+
+            label = visual.TextStim(win, '%s' % (grid_pos, ), pos=(stim_pos[0], stim_pos[1]+gabor_size/2), alignVert='bottom')
+            label.draw()
+
+        win.flip()
+        win.getMovieFrame()
+
+        output = path.join(output_dir, '{}Gems.png'.format(name))
+        win.saveMovieFrames(output)
+
+        if open_after:
+            ctx.run('open %s' % (output, ), echo=True)
 
 
 @task
@@ -54,54 +108,6 @@ def draw(ctx, name, open_after=False):
 
         if open_after:
             ctx.run('open {}'.format(output), echo=True)
-
-
-@task
-def gabors(ctx, name, output=None, open_after=False):
-    """Draw gabors sampled from the landscape.
-
-    Examples:
-
-        $ inv landscape.draw SimpleHill
-
-    """
-    from psychopy import visual
-    from numpy import linspace
-
-    win = visual.Window(size=(800, 800), units='pix', color=(0.6, 0.6, 0.6))
-
-    grid_size = 10
-    positions = linspace(0, 100, grid_size, endpoint=False, dtype='int')
-    grid_positions = list(product(positions, positions))
-
-    gabor_size = 60
-    positions = linspace(0, 100, grid_size, endpoint=False, dtype='int')
-    grid_positions = list(product(positions, positions))
-    stim_positions = gems.create_grid_positions(n_rows=grid_size, n_cols=grid_size,
-                                           win_size=win.size,
-                                           stim_size=gabor_size)
-
-    landscapes = get_landscapes_from_name(name)
-    for name, landscape in landscapes.items():
-        landscape.grating_stim_kwargs.update({'win': win, 'size': gabor_size})
-        gabors = landscape.get_grid_of_grating_stims(grid_positions)
-
-        for (grid_pos, stim_pos) in zip(grid_positions, stim_positions):
-            gabor = gabors[grid_pos]
-            gabor.pos = stim_pos
-            gabor.draw()
-
-            label = visual.TextStim(win, '%s' % (grid_pos, ), pos=(stim_pos[0], stim_pos[1]+gabor_size/2), alignVert='bottom')
-            label.draw()
-
-        win.flip()
-        win.getMovieFrame()
-
-        output = path.join(gems.config.LANDSCAPE_FILES, '{}Gems.png'.format(name))
-        win.saveMovieFrames(output)
-
-        if open_after:
-            ctx.run('open %s' % (output, ), echo=True)
 
 
 @task
