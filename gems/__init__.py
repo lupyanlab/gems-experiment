74c95e006d94097b048740d1b216fb623b1b4576
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-10-03 10:04:02 -0500

    Update figures task to draw v1.2 screen

--- gems/__init__.py
+++ gems/__init__.py
@@ -1,4 +1,4 @@
 from experiment import Experiment
 from landscape import Landscape, SimpleHill
-from display import create_radial_positions, create_grid_positions
+from display import create_radial_positions, create_grid_positions, create_line_positions
 from util import create_grid
